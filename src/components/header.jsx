ca211b95b210d08d7b7d2b9f9b07db77bcd3a526
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-03-27 11:46:18 +0200

    improve preact alias for parcel

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const [count, setCount] = useState(0)
   return (
     <div>
-      <p>click this button.</p>
+      <p>click this button</p>
       <button onClick={() => setCount(count + 1)}>button</button>
       <div>current count: {count}</div>
     </div>
